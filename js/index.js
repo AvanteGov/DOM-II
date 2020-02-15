2bce34f771bfdee56121ea36fe4dcc78301433b3
Alex <alexcouts18@gmail.com>
2020-02-15 17:37:30 -0500

    wrote some JS changes to imgs and nav elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+// adds color to nav links when mouse hovers 
+let hoverNav = document.querySelectorAll('.nav-link');
+
+hoverNav.forEach((element) => {
+    element.addEventListener('mouseover', (e) =>{
+        element.style.color = "green"
+        e.stopPropagation();
+    })
+})
+
+
+// images darken upon mouseover
+let images = document.querySelectorAll('img');
+
+images.forEach((element) => {
+    element.addEventListener('mouseenter', (e) => {
+        element.style.filter = "brightness(50%)";
+        e.stopPropagation();
+    })
+})
+
+
+// buttons read "let's go" upon mouse over
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((element) => {
+    element.addEventListener('mouseenter', (e) => {{
+        element.textContent = "Let's Go!";
+        e.stopPropagation();
+    }})
+})
+
